bcf1006080d86ed15a5201e374e03a3e80c15734
Nazariy2 <nazrud23@gmail.com>
2025-04-30 19:27:40 +0300

    a

---
+++ script.js
@@ -0,0 +1,40 @@
+// const sliderInput = document.querySelector('.slider__input');
+// const image = document.querySelector('.slider__image');
+
+// let debounceTimeout;
+
+// function debounce(fn, delay) {
+//   return function(...args) {
+//     clearTimeout(debounceTimeout);
+//     debounceTimeout = setTimeout(() => fn.apply(this, args), delay);
+//   };
+// }
+
+// function updateImageSize(value) {
+//   const size = 100 + (value - 50) * 3; 
+//   image.style.width = `${size}px`;
+// }
+
+// const debouncedResize = debounce((e) => {
+//   updateImageSize(e.target.value);
+// }, 150); 
+
+// sliderInput.addEventListener('input', debouncedResize);
+
+
+//2
+
+const box = document.getElementById('box');
+
+// Функція переміщення
+function moveBox(e) {
+  const x = e.clientX;
+  const y = e.clientY;
+  box.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+// Обгортка debounce з lodash (100мс)
+const debouncedMove = _.debounce(moveBox, 100);
+
+// Прив'язуємо рух миші до обробника
+document.addEventListener('mousemove', debouncedMove);
